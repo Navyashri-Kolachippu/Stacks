20d6988bad38062a9958c66ee1667a7187d06fbd
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-02-26 22:11:29 +0530

    change in stack

---
+++ Stacks/735Asteroid/Asteroid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks.Asteroid
+{
+    public class Asteroid
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (int asteroid in asteroids)
+            {
+                if (asteroid > 0)
+                    stack.Push(asteroid);
+                else
+                {
+                    while (stack.Count > 0 && stack.Peek() > 0 && stack.Peek() < Math.Abs(asteroid))
+                    {
+                        stack.Pop();
+                    }
+                    if (stack.Count > 0 && stack.Peek() == Math.Abs(asteroid))
+                    {
+                        stack.Pop();
+                    }
+                    else if (stack.Count == 0 || stack.Peek() < 0)
+                    {
+                        stack.Push(asteroid);
+                    }
+                }
+
+            }
+            return stack.Reverse().ToArray();
+
+        }
+    }
+}
--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -1,14 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Stacks._20ValidParanthesis;
+using Stacks.Asteroid;
 
-Valid valid = new Valid();
-//var a =valid.AsteroidCollision(new[] { 5, 10, -5 });
-//Console.WriteLine(string.Join(", ", a));
+Asteroid asteroid = new Asteroid();
+var a = asteroid.AsteroidCollision(new[] { 5, 10, -5 });
+Console.WriteLine(string.Join(", ", a));
 
-var b = valid.UniqueOccurrences(new[] { 1, 2, 2, 1, 1, 3 });
-Console.WriteLine(b);
-
-int[] nums = new[] { 1, 2, 3, 4, 5, 6, 7 };
-
-valid.Rotate(nums, 3);
